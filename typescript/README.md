c19cebaea487e3485fa576f1a960988acbcbf683
Bang Wu <ohwubang@gmail.com>
2020-08-16 18:04:13 +0800

    add the run single test case script

---
+++ typescript/README.md
@@ -0,0 +1,11 @@
+### User Guide
+## Run All Test Case:
+```
+npm run test
+```
+
+## Run Single Test Case:
+
+```
+npm run test --id=1
+```
--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
